3b297de22da9d1008ed5e7af1521a0f21867ee35
zeldaddicted <cynthiavanhees@hotmail.com>
2017-01-12 14:26:31 +0100

    Start 2.0

---
+++ EstrangePrototype2/Assets/Scripts/pickUpScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickUpScript : MonoBehaviour {
+
+    public int picturePieces = 0;
+
+    RaycastHit hit;
+
+    public GameObject[] picturePiece;
+
+    public AudioClip paperCrumble;
+
+    AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(picturePiece[0], new Vector3(0f, 1f, 0f), Quaternion.identity);
+        Instantiate(picturePiece[1], new Vector3(3f, 1f, 0f), Quaternion.identity);
+        Instantiate(picturePiece[2], new Vector3(-3f, 1f, 0f), Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Ray pickUp = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(transform.position, transform.forward * 50f);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics.Raycast(pickUp, out hit, 20f))
+            {
+                if (hit.collider.gameObject.CompareTag("PictureOne"))
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    GetComponent<AudioSource>().PlayOneShot(paperCrumble, 1f);
+                    picturePieces = picturePieces + 1;
+                }
+                if (hit.collider.gameObject.CompareTag("PictureTwo"))
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    picturePieces = picturePieces + 1;
+                }
+                if (hit.collider.gameObject.CompareTag("PictureThree"))
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    picturePieces = picturePieces + 1;
+                }
+            }
+        }
+    }
+}
